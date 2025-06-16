5eda00332177e7a1f7004d9753fb3d1a9b2098c8
SugayaShion <zeal24360050@fsgcl.jp>
2025-06-16 12:07:47 +0900

    10.3基本となるクラスを作成する

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,7 @@
 
-public class Player {
-
+public class Player extends Character{
+	public Player(double x, double y, double vx, double vy) {
+		//Characterクラスのコンストラクタを呼び出し
+		super(x, y, vx, vy);
+	}
 }
--- src/PlayerBullet.java
+++ src/PlayerBullet.java
@@ -1,4 +1,8 @@
 
-public class PlayerBullet {
+public class PlayerBullet extends Character{
+	public PlayerBullet(double x, double y, double vx, double vy) {
+		//Characterクラスのコンストラクタを呼び出し
+		super(x, y, vx, vy);
+	}
 
 }
