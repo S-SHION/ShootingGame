45b050c318fd87efb140b2a5d825fb3afeb61332
SugayaShion <zeal24360050@fsgcl.jp>
2025-06-17 12:20:42 +0900

    11.4 敵を出現させる

--- src/EnemyBase.java
+++ src/EnemyBase.java
@@ -1,5 +1,11 @@
 
 public class EnemyBase extends Enemy{
+	public void draw(MyFrame f) {
+		f.setColor(0, 180, 0);
+		f.fillOval(x, y, 32, 32);
+		f.setColor(200, 200, 200);
+		f.fillOval(x-16, y+8, 64, 16);
+	}
 	public EnemyBase(double x, double y, double vx, double vy) {
 		super(x, y, vx, vy);
 	}
@@ -10,6 +16,9 @@ public class EnemyBase extends Enemy{
 		}
 		if(x<100) {
 			vx = 1;
+		}
+		if (Math.random()<0.05) {
+			GameWorld.enemies.add(new StraightEnemy(x, y, 0, 2));
 		}
 	}
 
