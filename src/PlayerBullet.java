4d217f70e5e58dd764f1cc6478c0eb80c0a12f0c
SugayaShion <zeal24360050@fsgcl.jp>
2025-06-17 12:03:21 +0900

    弾の大きさを変更

--- src/PlayerBullet.java
+++ src/PlayerBullet.java
@@ -2,7 +2,7 @@
 public class PlayerBullet extends Character{
 	public void draw(MyFrame f) {
 		f.setColor(245, 0, 0);
-		f.fillOval(x+7, y-20, 15, 15);
+		f.fillOval(x+10, y-20, 10, 10);
 	}
 	public PlayerBullet(double x, double y, double vx, double vy) {
 		//Characterクラスのコンストラクタを呼び出し
